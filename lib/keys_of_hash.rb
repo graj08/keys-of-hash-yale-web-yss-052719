1e9547f2cb5c2a9af7ed79cac5fee3c9710ffbbf
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-23 02:13:16 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,6 @@
 class Hash
   def keys_of(*arguments)
-    return_array = Array.new 
+    return_array = Array.new
     Hash.each do |key,value|
       if value == arguments
         return_array << key
